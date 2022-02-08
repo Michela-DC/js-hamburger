c88adcf2b83034d758a6eda89e3bac3ca75fe578
Michela-DC <micheladelconte@outlook.com>
2022-02-08 17:15:37 +0100

    Close .hamburger-menu when click on .close

--- teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,24 +1,31 @@
-/* Quando clicko su 
-<a href="#">
-    <i class="fas fa-bars"></i>
-</a>
-compare pagina con class="hamburger-menu"
-*/
+//Quando clicko su <i class="fas fa-bars"></i> compare pagina con class="hamburger-menu", quindi:
+// --> devo usare addEventListener() per rilevare quando ci cliccano sopra
+// --> devo dire che .hamburger-menu da display = none diventa block quindi devo usare .style per riassegnare il display
 
-// Per farlo:
-// devo usare addEventListener() per rilevare quando ci cliccano sopra
-// devo dire che .hamburger-menu da display = none diventa block quindi devo usare .style per riassegnare il display
-
-
-const ICON_HAMBURGER = document.querySelector(".fa-bars")
-console.log(ICON_HAMBURGER)
+const OPEN_HAMBURGER = document.querySelector(".fa-bars")
+console.log(OPEN_HAMBURGER)
 
 const HAMBURGER_MENU = document.querySelector(".hamburger-menu")
 console.log(HAMBURGER_MENU)
 
-ICON_HAMBURGER.addEventListener('click', 
+OPEN_HAMBURGER.addEventListener('click', 
 
     function() {
         HAMBURGER_MENU.style.display = 'block'
     }
-);
+)
+
+// Quando clicco su .close si chiude .hamburger-menu, quindi:
+// -->  .addEventListener() quando clicco su .close
+// -->  HAMBURGER_MENU va in display = none
+
+const CLOSE_HAMBURGER = document.querySelector(".close")
+console.log(CLOSE_HAMBURGER)
+
+CLOSE_HAMBURGER.addEventListener('click',
+    
+    function() {
+    HAMBURGER_MENU.style.display = 'none'
+}
+)
+
